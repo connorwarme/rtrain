4628518d8d4d172c4d6e5f5c7f5db712c34db1d2
connorwarme <connor.warme@gmail.com>
2024-02-05 11:49:41 -0700

    Simple manage state exercise

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import List from "./List";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [bgColor, setBgColor] = useState('red')
 
   const handleCount = () => {
     setCount(count + 1);
@@ -17,13 +18,28 @@ function App() {
     setCount(0);
   };
 
+  const changeBg = () => {
+    if (bgColor === 'red') {
+      setBgColor('blue')
+    } else if (bgColor === 'blue') {
+      setBgColor('purple')
+    } else if (bgColor === 'purple') {
+      setBgColor('white')
+    } else {
+      setBgColor('red')
+    }
+    handleCount()
+  }
+
   return (
     <>
       <Header />
       <Greeting />
+      <div style={{backgroundColor: bgColor, height: '100px', width: '100px'}}></div>
       <p>Count is: {count}</p>
       <button onClick={handleCount}>Add to Count</button>
       <button onClick={clearCount}>Clear Count</button>
+      <button onClick={changeBg}>Change Color</button>
       <div>
         <Testimony />
       </div>
