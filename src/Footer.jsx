fc83b042405f4fc7a6d462ab1124479132057642
connorwarme <connor.warme@gmail.com>
2024-02-06 19:50:10 -0700

    Build simple footer comp

---
+++ src/Footer.jsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+
+
+  return ( 
+    <>
+      <div className="footer-container">
+        <div className="footer-content">
+          <p>Built by Connor Warme</p>
+          <a href="https://github.com/connorwarme">GitHub</a>
+        </div>
+      </div>
+    </>
+   );
+}
+ 
+export default Footer;
