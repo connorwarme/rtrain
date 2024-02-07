88c5f7f5632d657df020c92b9beffe64f934b34a
connorwarme <connor.warme@gmail.com>
2024-02-07 12:32:50 -0700

    Build out html structure, following tailwind tutorial

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import Nav from "./components/Nav";
+import Header from "./components/Header";
 import "./App.css";
 
 
@@ -8,6 +10,42 @@ function App() {
   return (
     <>
       <Nav />
+
+      <main>
+        <div>
+          <Link to={'/login'}>Log in</Link>
+          <Link to={'signup'}>Sign up</Link>
+        </div>
+
+        <header>
+          <Header />
+        </header>
+
+        <div>
+          <h4>Latest Recipes</h4>
+    
+          <div>
+            {/* <!-- cards go here --> */}
+            <div> 
+              <img src="img/stew.jpg" alt="stew" />
+              <div>
+                <span>5 Bean Chili Stew</span>
+                <span>Recipe by Mario</span>
+              </div>
+            </div>
+          </div>
+
+          <h4>Most Popular</h4>
+
+          <div>
+            {/* <!-- cards go here --> */}
+          </div>
+        </div>
+
+        <div>
+          <div>Load more</div>
+        </div>    
+      </main>
     </>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return ( 
+    <>
+      <h2>Recipes</h2>
+      <h3>For Ninjas</h3>
+    </>
+   );
+}
+ 
+export default Header;
