6aaef2ad0979d65198611efb54f51a81d648581b
connorwarme <connor.warme@gmail.com>
2024-02-07 12:06:11 -0700

    Install react-router, implement, build Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,31 +1,34 @@
-
+import { Link } from "react-router-dom";
+import NavLink from "./NavLink";
 
 const Nav = () => {
+
+  const links = [
+    {
+      text: 'Home',
+      location: '/'
+    },
+    {
+      text: 'About',
+      location: '/about'
+    },
+    {
+      text: 'Contact',
+      location: '/contact'
+    },
+  ]
+
   return ( 
     <>
       <div>
         <nav>
           <div>
             <h1>
-              <a href="/">Food Ninja</a>
+              <Link to={"/"}>Food Ninja</Link>
             </h1>
           </div>
           <ul>
-            <li>
-              <a href="#">
-                <span>Home</span>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <span>About</span>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <span>Contact</span>
-              </a>
-            </li>
+            { links.map((link, index) => <NavLink key={index} link={link} />)}
           </ul>
         </nav>
       </div>
