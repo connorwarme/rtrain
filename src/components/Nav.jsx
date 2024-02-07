e97b6fa2cd1394c4072773c53018c5d4948fedf8
connorwarme <connor.warme@gmail.com>
2024-02-07 16:41:41 -0700

    Add menu icon for small vw, hide for wider screens

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import NavLink from './NavLink'
 import home from "../assets/home.svg"
@@ -5,36 +6,50 @@ import about from "../assets/about.svg"
 import contact from "../assets/contact.svg"
 
 const Nav = () => {
+
   const links = [
     {
       text: 'Home',
       location: '/',
       img: home,
+      current: true,
     },
     {
       text: 'About',
       location: '/about',
       img: about,
+      current: false,
     },
     {
       text: 'Contact',
       location: '/contact',
       img: contact,
+      current: false,
     },
   ]
 
+  const handleMenuClick = () => {
+    const list = document.querySelector('#menu-list')
+    list.classList.toggle('hidden')
+  }
+
   return (
     <>
-      <div className="md:col-span-1">
-        <nav>
-          <div className="border-b border-gray-200 p-4">
-            <h1>
-              <Link to={'/'} className="font-bold uppercase hover:text-gray-800">
+      <div className="md:col-span-1 md:flex md:justify-end">
+        <nav className='text-right'>
+          <div className="flex justify-between items-center">
+            <h1 className='border-b border-gray-200 p-4 font-bold uppercase'>
+              <Link to={'/'} className="hover:text-gray-800">
                 Food Ninja
               </Link>
             </h1>
+            <div className='px-4 cursor-pointer md:hidden' id='menu' onClick={handleMenuClick}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+              </svg>
+            </div>
           </div>
-          <ul>
+          <ul className='text-sm mt-6 hidden md:block' id='menu-list'>
             {links.map((link, index) => (
               <NavLink key={index} link={link} />
             ))}
--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom'
 
 const NavLink = ({ link }) => {
+  // would be cool to figure out how to dynamically update this when use goes to other pages...
+  const linkClass = link.current ? 'border-primary' : 'border-white'
   return (
     <>
-      <li className="font-bold text-gray-700">
-        <Link to={link.location} className='flex items-center'>
+      <li className="font-bold text-gray-700 py-1">
+        <Link to={link.location} className={'px-4 flex justify-end items-center border-r-4' + ' ' + linkClass}>
           <img className='w-5 ml-2 mr-2' src={link.img} alt="" />
           <span>{link.text}</span>
         </Link>
