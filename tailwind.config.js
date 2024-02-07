49c2d6eab1b8460705f3f9be5fe6a2f05d2b8cd3
connorwarme <connor.warme@gmail.com>
2024-02-07 13:26:19 -0700

    Implement extending default config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,15 @@ export default {
     './src/components/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        }
+      }
+    },
   },
   plugins: [],
 }
