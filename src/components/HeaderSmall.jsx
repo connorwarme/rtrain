75f1b8b066a3972917ad1c52a0b03e1bce818139
connorwarme <connor.warme@gmail.com>
2024-02-09 19:35:08 -0700

    Remove old content for new branch

--- src/components/HeaderSmall.jsx
+++
@@ -1,9 +0,0 @@
-const HeaderSmall = ({ text }) => {
-  return (
-    <>
-      <h4 className="mt-12 border-b border-gray-400 pb-2 font-bold">{text}</h4>
-    </>
-  )
-}
-
-export default HeaderSmall
