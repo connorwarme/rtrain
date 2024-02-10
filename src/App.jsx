1a7ff54ec7f26c62641a0f44ad3669c9c00544ba
connorwarme <connor.warme@gmail.com>
2024-02-09 19:38:44 -0700

    Cleaning up

--- src/App.jsx
+++ src/App.jsx
@@ -1,91 +1,13 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import Nav from './components/Nav'
-import Header from './components/Header'
-import HeaderSmall from './components/HeaderSmall'
-import Card from './components/Card'
-import Footer from './components/Footer'
-import stew from './assets/img/stew.jpg'
-import noodles from './assets/img/noodles.jpg'
-import curry from './assets/img/curry.jpg'
+
 import './App.css'
 
 function App() {
-  const recipes = [
-    {
-      title: '5 Bean Chili Stew',
-      img: {
-        url: stew,
-        alt: 'Stew',
-      },
-      author: 'Mario',
-      duration: '25',
-    },
-    {
-      title: 'Veg Noodles',
-      img: {
-        url: noodles,
-        alt: 'Noodles',
-      },
-      author: 'Mario',
-      duration: '20',
-    },
-    {
-      title: 'Tofu Curry',
-      img: {
-        url: curry,
-        alt: 'Curry',
-      },
-      author: 'Mario',
-      duration: '15',
-    },
-  ]
+
   return (
-    <div className="md:grid font-body md:grid-cols-3">
-      <Nav />
-
-      <main className="bg-gray-100 px-16 py-6 md:col-span-2">
-        <div className="flex justify-center md:justify-end">
-          <Link
-            to={'/login'}
-            className="btn border-primary text-primary transition duration-500 ease-out hover:bg-primary hover:text-white md:border-2"
-          >
-            Log in
-          </Link>
-          <Link
-            to={'signup'}
-            className="btn ml-2 border-primary text-primary transition duration-500 ease-out hover:bg-primary hover:text-white md:border-2"
-          >
-            Sign up
-          </Link>
-        </div>
-
-        <header>
-          <Header />
-        </header>
-
-        <div>
-          <HeaderSmall text="Latest Recipes" />
-
-          <div className="mt-8 grid gap-10 lg:grid-cols-3">
-            {/* <!-- cards go here --> */}
-            {recipes.map((recipe, index) => (
-              <Card key={index} recipe={recipe} />
-            ))}
-          </div>
-
-          <HeaderSmall text="Most Popular" />
-
-          <div className="mt-8">{/* <!-- cards go here --> */}</div>
-        </div>
-
-        <div className="mt-12 flex justify-center">
-          <div className="btn transform bg-secondary-100 text-secondary-200 transition duration-300 ease-out hover:scale-125 hover:bg-opacity-50 hover:shadow-inner">
-            Load More
-          </div>
-        </div>
-      </main>
-      <Footer />
+    <div>
+      
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
-import ErrorPage from './ErrorPage.jsx'
+// import ErrorPage from './ErrorPage.jsx'
 import './index.css'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    errorElement: <ErrorPage />,
+    // errorElement: <ErrorPage />,
   },
 ])
 
