79a8bc2cc55c3bcb477fedf2b6e8af866129f320
connorwarme <connor.warme@gmail.com>
2024-02-10 20:02:30 -0700

    Implement a star rating system

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Accordion from './components/accordion'
 import RandomColor from './components/color'
 
 import './App.css'
+import StarRating from './components/rating'
 
 function App() {
   return (
     <div>
       {/* <Accordion /> */}
-      <RandomColor />
+      {/* <RandomColor /> */}
+      <StarRating />
     </div>
   )
 }
---
+++ src/components/rating/index.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import home from "../../assets/home.svg"
+
+const StarRating = ({ starNumber = 10 }) => {
+  const [rating, setRating] = useState(0)
+  const [hover, setHover] = useState(0)
+
+  const handleClick = (index) => {
+    setRating(index)
+    console.log(index)
+  }
+
+  const handleEnter = (index) => {
+    setHover(index)
+    console.log(index)
+  }
+  
+  const handleLeave = () => {
+    setHover(rating)
+  }
+
+  return ( 
+    <>
+      {[...Array(starNumber)].map((star, index) => {
+        index += 1
+        return (
+          <div key={index} onClick={() => handleClick(index)} onMouseEnter={()=>handleEnter(index)} onMouseLeave={() => handleLeave()}>
+            <img src={home} alt="" className={ index <= (hover || rating) ? 'bg-slate-400' : 'bg-red-500'} style={{height: '40px'}}/>
+          </div>
+        )
+      })}
+    </>
+   );
+}
+ 
+export default StarRating;
